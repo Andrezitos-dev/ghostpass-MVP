e55074223f719a550275dfdc1f0051a070c9453a
Andrezitos-dev <andreluissoaresramlho@gmail.com>
2026-04-16 15:47:46 -0300

    creating password
    O código responsável pela geração aleatória de senhas.

---
+++ principal.py
@@ -0,0 +1,12 @@
+import secrets
+import string 
+
+# Definir o que pode ter na sua senha
+caracteres =string.ascii_letters + string.digits * 5 + string.punctuation
+# gerar uma senha aleatória 
+senha = ''.join(secrets.choice(caracteres)) 
+
+for i in range (0, 15):
+    senha += secrets.choice(caracteres)
+
+print(f"Sua senha gerada aleatoriamente é: {senha}")
