46fd511825ed9d6714a03a80b3d3b0889a1406d6
Andrezitos-dev <andreluissoaresramlho@gmail.com>
2026-04-27 21:37:05 -0300

    nova nome+correção
    Tentativa de correção de bug. E troca de nome de principal por servidor.

--- servidor.py
+++ servidor.py
@@ -38,6 +38,16 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/api/identidade", methods=["GET"])
+def identidade():
+    novo_email = criar_email()
+    nova_senha = gerar_senha(16)
+
+    return jsonify({
+        "email": novo_email,
+        "senha": nova_senha
+    })
+
 #🔹criar email 
 @app.route("/api/email/", methods=["GET"] )
 def email():
